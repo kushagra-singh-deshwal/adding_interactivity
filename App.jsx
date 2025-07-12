1cadc548ba008e69a1a457f6cd7a8f82ad70e2df
sky_watch <kushagrasingh048@gmail.com>
2025-07-13 04:03:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+import List from './components/ToDoList'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <List/>
+    </>
+  )
+}
+
+export default App
